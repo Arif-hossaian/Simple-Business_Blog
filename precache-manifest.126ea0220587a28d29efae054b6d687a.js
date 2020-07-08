764fcb12098c07ac7ff0cf32ff1a107cda6d575a
arifhossain123 <arifhossain42136@gmail.com>
2020-07-08 21:32:40 +0600

    Updates

---
+++ precache-manifest.126ea0220587a28d29efae054b6d687a.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35a554e0d6f6fd41d6cc0e530f84946f",
+    "url": "/Simple-Business_Blog/index.html"
+  },
+  {
+    "revision": "508ab39614bd2e2f7600",
+    "url": "/Simple-Business_Blog/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "ab736b9de0e017e8972a",
+    "url": "/Simple-Business_Blog/static/css/main.c095396d.chunk.css"
+  },
+  {
+    "revision": "508ab39614bd2e2f7600",
+    "url": "/Simple-Business_Blog/static/js/2.3a53f7b3.chunk.js"
+  },
+  {
+    "revision": "beb67ca59693e0ee743e38777a9f8025",
+    "url": "/Simple-Business_Blog/static/js/2.3a53f7b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab736b9de0e017e8972a",
+    "url": "/Simple-Business_Blog/static/js/main.d64e8484.chunk.js"
+  },
+  {
+    "revision": "013db1c7756414b83fbf",
+    "url": "/Simple-Business_Blog/static/js/runtime-main.83b2e091.js"
+  },
+  {
+    "revision": "d785dd089fa9e7bd471f3359f697ffab",
+    "url": "/Simple-Business_Blog/static/media/android_dev.d785dd08.jpg"
+  },
+  {
+    "revision": "9ce8aa204a21e69716018fee472b042c",
+    "url": "/Simple-Business_Blog/static/media/app_dev.9ce8aa20.png"
+  },
+  {
+    "revision": "2b44c33c81df088bd6032e7267366aba",
+    "url": "/Simple-Business_Blog/static/media/digital.2b44c33c.jpeg"
+  },
+  {
+    "revision": "16ca8773e4fbf2ab973b97b1deb91f69",
+    "url": "/Simple-Business_Blog/static/media/graphics.16ca8773.jpg"
+  },
+  {
+    "revision": "4bd6a8ec30dbaefa027cc48455751777",
+    "url": "/Simple-Business_Blog/static/media/iphn web image for service.4bd6a8ec.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Simple-Business_Blog/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "6a28489cc22d5a8e9561b8347284ade2",
+    "url": "/Simple-Business_Blog/static/media/software_dev.6a28489c.jpg"
+  },
+  {
+    "revision": "0ff1d0f44473d3d7feada1f723767902",
+    "url": "/Simple-Business_Blog/static/media/success.0ff1d0f4.svg"
+  },
+  {
+    "revision": "e18f4c859d383b1dded81d5543e6f456",
+    "url": "/Simple-Business_Blog/static/media/web_development.e18f4c85.jpg"
+  }
+]);
